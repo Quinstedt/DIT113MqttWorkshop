dc035752d416246317fb2442b78ef078be8d1283
Quinstedt <90027419+Quinstedt@users.noreply.github.com>
2023-03-28 04:24:47 +0200

    adds student template repo

--- SpeechToText/app/src/main/java/com/quinstedt/speechtotext/Colors.java
+++ SpeechToText/app/src/main/java/com/quinstedt/speechtotext/Colors.java
@@ -1,6 +1,7 @@
 package com.quinstedt.speechtotext;
 /* TODO
     Add at least one more colors
+    Choice from the list: https://wiki.seeedstudio.com/Wio-Terminal-LCD-Basic
  */
 public enum Colors {
     BLUE("blue"),
